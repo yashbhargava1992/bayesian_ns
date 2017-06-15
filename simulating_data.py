0acef6f569ba67f47c1ccad80cf7964a8165c852
Yash Bhargava <yash.bhargava1992@gmail.com>
2017-06-15 22:52:34 +0530

    update_code

--- simulating_data.py
+++ simulating_data.py
@@ -1,30 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import numpy.random as rnd
-pi = np.pi
+import my_function as mf
 
-
-def damped_sin(t,tau,freq,amp=None,phase=None):
-	if amp==None: amp=1
-	if phase==None: phase=0
-	y = amp*np.exp(-t/tau)*np.sin(2*pi*freq*t+phase)
-	#~ y = amp*np.sin(2*pi*freq*t+phase)
-	return y
-	
 
 
 t = np.arange(0,10,0.0001)
-sig = damped_sin(t,1,1010)
+sig = mf.damped_sin(t,1,1010)
 
 
-for x in range(5):
+for x in range(1):
 	noise = rnd.normal(0,1,len(t))
-	np.savetxt("sim_data/data_{}.txt".format(x),np.transpose([t,sig+noise]),fmt=['%.5f','%.5f'])
+	np.savetxt("sim_data/noiseless_data_{}.txt".format(x),np.transpose([t,sig]),fmt=['%.5f','%.5f'])
 
-	plt.plot(t,sig+noise,'.',label='Signal+Noise')
+	#plt.plot(t,sig+noise,'.',label='Signal+Noise')
 	plt.plot(t,sig,'-',label='Signal')
 	plt.legend()
-	plt.savefig('sim_data/signal_realisation_{}.png'.format(x))
+	plt.savefig('sim_data/noiseless_signal_realisation_{}.png'.format(x))
 	
 	#~ plt.show()
 	plt.clf()
