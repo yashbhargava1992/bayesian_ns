888182080928bb80a660d72b1dd1e82f516f7b36
Yash <yash.bhargava1992@gmail.com>
2017-07-05 00:55:42 +0530

    Updating codes

---
+++ mcmc_actual_data_sim.py
@@ -0,0 +1,116 @@
+
+from __future__ import division
+import numpy as np
+import matplotlib.pyplot as plt
+import numpy.random as rnd
+import my_function as mf
+import argparse 
+import emcee as mc
+import time
+import corner_local as cr
+
+
+
+#~ pr = argparse.ArgumentParser()
+#~ pr.add_argument("guess",type=float,default=500)
+#~ pr.add_argument("-i","--iter",type =int, default = 500)
+#~ pr.add_argument("-w","--width",type= float,default = 100)
+#~ args = pr.parse_args()
+
+start_time = time.time()
+#~ data = np.loadtxt("sim_data/data_0.txt",unpack=True)
+#~ t = data[0]
+#~ s = data[1]
+
+date_append = "Jul04"
+
+t = np.linspace(0,0.5, 100000)
+
+seed_freq 	= 2300
+seed_tau 	= 0.02345
+seed_amp 	= 1
+seed_gamma	= 38
+seed_xi		= -9e2
+
+guess = np.array([2000,0.02,1,-1000,40]) ## Format of guess is freq,tau,amp,gamma,xi
+width = guess/10
+iters = 100
+num_bins = 50
+
+
+sig = mf.f2_sin(t,[seed_tau,seed_freq,seed_amp,seed_gamma,seed_xi,0.1])
+noise = rnd.normal(0,1,len(t))
+samp = sig+noise
+#~ print sig
+#~ print np.shape(sig)
+
+ax1 = plt.subplot(211)
+ax1.plot(t,sig,'-')
+ax2 = plt.subplot(212)
+ax2.plot(t,samp,'-')
+#~ plt.show()
+plt.clf()
+
+
+
+dt = t[1]-t[0]
+nyq_freq = 0.5/dt
+xf = np.linspace (0,nyq_freq,len(t)/2)
+df = xf[1]-xf[0]
+
+current_time = time.time()
+ndim,nwalkers = 5,100
+
+
+
+np.savetxt("test_actual_data_{}.txt".format(date_append),np.transpose([t,samp]),fmt=['%.5f','%.5f'],
+			header="Freq={}\ttau={}\tamp={}\tgamma={}\txi={}".format(seed_freq,seed_tau,seed_amp,seed_gamma,seed_xi))
+
+pos = [guess+width*rnd.randn(ndim) for i in range (nwalkers)]
+
+sampler = mc.EnsembleSampler(nwalkers,ndim,mf.lnprob,args = (t,samp,mf.f2_sin),threads=4)
+sampler.run_mcmc(pos, iters)
+current_time1 = time.time()
+print "EMCEE done, time required:\t", current_time1-current_time
+
+print np.shape(sampler.chain)
+freq_list = sampler.chain[:, 50:, 0].flatten()
+tau_list = sampler.chain[:, 50:, 1].flatten()
+amp_list = sampler.chain[:, 50:, -1].flatten()
+ax1 = plt.subplot(311)
+ax2 = plt.subplot(312)
+ax3 = plt.subplot(313)
+ax1.hist(freq_list,100)
+#ax2.hist(tau_list,100)
+#ax3.hist(amp_list,100)
+freq_bins = np.linspace(np.min(freq_list),np.max(freq_list),num_bins)
+tau_bins = np.logspace(-3,np.log10(max(tau_list)),num_bins)
+amp_bins = np.logspace(-3,np.log10(max(amp_list)),num_bins)
+ax2.hist(tau_list,tau_bins)
+ax3.hist(amp_list,amp_bins)
+ax1.set_ylabel("Freq hist")
+
+ax2.set_ylabel(r"$\tau$ hist")
+
+ax3.set_ylabel("Amp hist")
+
+hist_2d,x_ed,y_ed = np.histogram2d(freq_list,tau_list,bins=(num_bins,num_bins))
+#ax3.hist2d(freq_list,tau_list,100)
+X,Y = np.meshgrid(x_ed,y_ed)
+#ax3.pcolormesh(X,Y,hist_2d.T)
+#plt.colorbar()
+#~ print np.shape(freq_list)
+with open ("mcmc_freq_tau_amp_list_{}.txt".format(date_append),'w') as f: np.savetxt(f,np.transpose([freq_list,tau_list,amp_list]),fmt=['%.3f','%.3f','%.3f'])
+plt.savefig("mcmc_3d_{}.png".format(date_append))
+#~ plt.show()
+plt.clf()
+
+
+samples = sampler.chain[:, 50:, :].reshape((-1, ndim))
+print "Shape of samples :\t", np.shape(samples)
+fig = cr.corner(samples, labels=["$freq$", r"$\tau$", "$A$", r"$\gamma$", r"$\xi$"],
+                      truths=[seed_freq, seed_tau, seed_amp, seed_gamma,seed_xi],
+                      bins = [num_bins,tau_bins,amp_bins,num_bins,num_bins],
+                      fill_contours=True,show_title=True,hist_2d_cmap=plt.get_cmap('cool'),
+                      quantiles=[0.05,0.5,0.95],verbose=True,color='r')
+fig.savefig("triangle_actual_data_{}.pdf".format(date_append))
--- my_function.py
+++ my_function.py
@@ -6,7 +6,7 @@ def log_Poisson (D,N):
 	#~ return D**N*np.exp(-D)/math.factorial(N)
 	return N*np.log(D)-D
 	
-def damped_sin(t,tau,freq,amp=1,t_start=0):
+def damped_sin(t,theta):
 	"""
 	
 	Function to generate a damped sinusoidal signal for any time 't' with supplied 
@@ -16,10 +16,11 @@ def damped_sin(t,tau,freq,amp=1,t_start=0):
 	Input 
 	----------------
 	t				: Time at which signal is required (in sec)
-	tau				: Damping timescale of signal (in sec)
-	freq    		: Frequency of oscillation (in Hz)
-	amp				: (Optional) Amplitude of signal. Default value 1
-	t_start			: (Optional) Time stamp of when the signal begins. Default value 0
+	theta 			: can consist of following
+		tau				: Damping timescale of signal (in sec)
+		freq    		: Frequency of oscillation (in Hz)
+		amp				: (Optional) Amplitude of signal. Default value 1
+		t_start			: (Optional) Time stamp of when the signal begins. Default value 0
 	
 	Output
 	----------------
@@ -30,7 +31,14 @@ def damped_sin(t,tau,freq,amp=1,t_start=0):
 	Due to ordering of arguments the case of optional amplitude and given t_start cannot be used
 		
 	"""
-	
+	if len(theta)==2:
+		freq,tau = theta
+		amp = 1
+		t_start = 0
+	elif len(theta)==3:
+		freq,tau,amp = theta
+		t_start = 0
+	else: freq,tau,amp,t_start = theta
 	# Defining default value
 	#if amp==None: amp=1
 	#if t_start==None: t_start=0
@@ -39,18 +47,47 @@ def damped_sin(t,tau,freq,amp=1,t_start=0):
 	y[ind] = amp*np.exp(-(t[ind]-t_start)/tau)*np.sin(2*pi*freq*(t[ind]-t_start))
 	return y
 
-def log_likelihood_v1 (t_k,N_k,*par):
+def f2_sin (t,theta):
+#~ tau,freq,gamma=38,xi=-9e2,amp=1,beta=0,t_start=0):
+	"""
+	
+	Parameters
+	----------------
+	t				: Time series over which signal is computed
+	theta 			: can consist of following
+		tau			    : Damping constant of signal
+		freq			: Base frequency of the signal
+		amp			    : Amplitude of the signal (Default value=1)
+		gamma           : Coefficient of t^2 term inside the sin (Default value=38 for GNH3)
+		xi				: Coefficient of t^3 term inside the sin (Default value=-9e2 for GNH3)
+		beta			: Phase term inside the sin (Default value=0)
+		t_start			: Start point of signal as compared to start of time series (Default value=0, assumes t_start>=t[0])
+	
+	
+	Returns
+	-----------------
+	y				: Signal comprising of only f2 term in  eq of h+ from Bose et al. 2017
 	
 	"""
-	Assumes a poisson process for generating counts. Not possible if we have Gaussian noise of zero mean
-	"""
-	if len(par)==2: tau,freq,amp,phase = par[0],par[1],1,0
-	elif len(par)==3: tau,freq,amp,phase = par[0],par[1],par[2],0
-	elif len(par)==4: tau,freq,amp,phase = par[0],par[1],par[2],par[3]
-	D = damped_sin(t_k,tau,freq,amp,phase)
-	ll = np.sum(log_Poisson(D,N_k))
-	return -ll
+	freq,tau,gamma,xi,amp,beta,t_start=1000,1,38,-9e2,1,0,0
+	if len(theta)==2:
+		freq,tau = theta
+	elif len(theta)==3:
+		freq,tau,amp = theta
+	elif len(theta)==5:
+		freq,tau,amp,gamma,xi = theta 
+	elif len(theta)==6:
+		freq,tau,amp,gamma,xi,t_start = theta 
+		print freq,tau,amp,gamma,xi,t_start 
 	
+	
+	ind = np.where (t>=t_start)
+	y = np.zeros(len(t))
+	y[ind] = amp*np.exp(-t[ind]/tau)*np.sin(2*pi*(freq*t[ind]+gamma*t[ind]**2+xi*t[ind]**3)+pi*beta)
+	
+	return y
+
+
 def double_sin(t,freq1,freq2,amp1=1,amp2=1):
 	"""
 	
@@ -108,6 +145,20 @@ def single_sin(t,freq1,amp1=1):
 	# Defining default value
 	y = amp1*np.sin(2*pi*freq1*(t)) 
 	return y
+
+
+def log_likelihood_v1 (t_k,N_k,*par):
+	
+	"""
+	Assumes a poisson process for generating counts. Not possible if we have Gaussian noise of zero mean
+	"""
+	if len(par)==2: tau,freq,amp,phase = par[0],par[1],1,0
+	elif len(par)==3: tau,freq,amp,phase = par[0],par[1],par[2],0
+	elif len(par)==4: tau,freq,amp,phase = par[0],par[1],par[2],par[3]
+	D = damped_sin(t_k,tau,freq,amp,phase)
+	ll = np.sum(log_Poisson(D,N_k))
+	return -ll
+	
 
 def prior_info_v1 (theta):
 	
@@ -189,8 +240,6 @@ def mcmc_1d(t,samp,init_guess = 2000,iter_number = 1e4,step = 100, multi_thres =
 
 	
 
-
-
 def likelihood_estimator (sf,h,df):
 	"""
 	Input 
@@ -210,6 +259,8 @@ def likelihood_estimator (sf,h,df):
 	
 	return ll
 
+
+
 def lnlike_v1 (theta,t,s):
 	dt 	= t[1]-t[0]
 	nyq_freq = 0.5/dt
@@ -227,23 +278,46 @@ def prior_info_dd (theta):
 	if len(theta)==2:
 		freq,tau = theta
 		if freq > 0 and tau > 0: 
-			return 0
+			return 0.0
 
 	elif len(theta)==3 : 
-		freq,tau,amp = theta
-		if freq > 0 and tau > 1e-10 and tau < 6 and amp > 1e-10 and amp < 4: 
-			return 0
+		freq 	= theta[0]
+		tau 	= theta[1]
+		amp 	= theta[2]
+		if freq > 0 and tau > 1e-10 and tau < 6 and amp > 1e-10 and amp < 15: 
+			return 0.0
+	elif len(theta)==5:
+		freq 	= theta[0]
+		tau 	= theta[1]
+		amp 	= theta[2]
+		gamma	= theta[3]
+		xi		= theta[4]
+		if ((freq>1500 and freq<3100) and 
+			(tau>1e-4 and tau < 1e-1) and
+			(gamma>-4000 and gamma<500) and
+			(xi >-1000 and xi< 1e5) and
+			(amp>1e-5 and amp<20)):
+				return 0.0
 	return -np.inf
 
+
 def lnlike_dd (theta,t,s,func):
-	if len(theta)==2:freq,tau = theta
-	elif len(theta)==3 : freq,tau,amp = theta
+	#~ if len(theta)==2:
+		#~ freq,tau = theta
+		#~ amp = 1
+		#~ gamma = None
+		#~ xi = None	
+	#~ elif len(theta)==3 : 
+		#~ freq,tau,amp = theta
+		#~ gamma = None
+		#~ xi = None
+	#~ elif len(theta)==5 : freq,tau,gamma,xi,amp = theta
 	dt 	= t[1]-t[0]
 	nyq_freq = 0.5/dt
 	xf 	= np.linspace (0,nyq_freq,len(t)/2)
 	df 	= xf[1]-xf[0]
 	sf 	= np.fft.rfft(s)
-	h 	= func(t,tau,freq,amp)
+	h 	= func(t,theta)
 	hf 	= np.fft.rfft(h)
 	sh 	= 0.5*( sf*np.conjugate(hf)+ hf*np.conjugate(sf)-hf*np.conjugate(hf))
 	ll 	= np.real(np.sum(sh)*df)
