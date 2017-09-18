3a6fba2dece8393742241310bba025f2b3f4805c
Yash Bhargava <yash.bhargava1992@gmail.com>
2017-09-18 18:18:37 +0530

    Updating code to make it more readable

--- mcmc_code.py
+++ mcmc_code.py
@@ -1,4 +1,11 @@
 
+## Code written for MCMC sampling for the Bayesian parameter estimation of 
+# post-merger gravitational wave signal of the merger of 2 Neutron stars
+
+### Author: 		Yash Bhargava, IUCAA 
+### Last updated: 	September 18, 2017
+
+
 from __future__ import division
 import numpy as np
 import matplotlib.pyplot as plt
@@ -24,24 +31,14 @@ pr.add_argument("xi",type=float,default=-9e2)
 pr.add_argument("dim",type=int,default=5)
 
 
-#~ pr.add_argument("-i","--iter",type =int, default = 500)
-#~ pr.add_argument("-w","--width",type= float,default = 100)
 args = pr.parse_args()
 
-start_time = time.time()
-#~ data = np.loadtxt("sim_data/data_0.txt",unpack=True)
-#~ t = data[0]
-#~ s = data[1]
+start_time = time.time()			# Timing the operations
 
-date_append = args.append_text
+date_append = args.append_text		# Text which will be appended to the files 
 
-t = np.linspace(0,0.5, 10000)
+t = np.linspace(0,0.5, 10000)		# Time duration considered for the signal
 
-#seed_freq 	= 2.42e3
-#seed_tau 	= 0.01037
-#seed_amp 	= 0.5
-#seed_gamma	= -3467
-#seed_xi		= 2e4
 
 seed_freq 	= args.freq
 seed_tau 	= args.tau
@@ -52,34 +49,31 @@ num_bins = 50
 
 
 if args.dim==3:
-	guess = np.array([2000,0.02,3])#,100,-1e3]) ## Format of guess is freq,tau,amp,gamma,xi
+	guess = np.array([2000,0.02,3])				## Format of guess is freq,tau,amp
 	labels = ["$f$", r"$\tau$", "$A$"]
 	truths=[seed_freq, seed_tau, seed_amp]
 	
 elif args.dim==2:
-	guess = np.array([2000,0.02])#,100,-1e3]) ## Format of guess is freq,tau,amp,gamma,xi
+	guess = np.array([2000,0.02]) 				## Format of guess is freq,tau
 	labels = ["$f$", r"$\tau$"]
 	truths=[seed_freq, seed_tau]
 elif args.dim==5:
-	guess = np.array([2000,0.02,3,100,-1e3])
+	guess = np.array([2000,0.02,3,100,-1e3])  	## Format of guess is freq,tau,amp,gamma,xi
 	labels=["$f$", r"$\tau$", "$A$",r"$\gamma$", r"$\xi$"]
 	
 	truths=[seed_freq, seed_tau, seed_amp, seed_gamma,seed_xi]
-width = guess/2
-iters = 1e3
 
 
 
 
+######## Generating the mock signal
 
 sig = mf.f2_sin(t,[seed_freq,seed_tau,seed_amp,seed_gamma,seed_xi])
 noise = rnd.normal(0,1,len(t))
 samp = sig+noise
-#print t
-#print sig
-print np.mean(noise), np.std(noise)
-#~ print np.shape(sig)
 
+
+## Plotting the mock signal
 ax1 = plt.subplot(211)
 ax1.plot(t,sig,'.')
 ax2 = plt.subplot(212)
@@ -95,36 +89,25 @@ xf = np.linspace (0,nyq_freq,len(t)/2)
 df = xf[1]-xf[0]
 
 current_time = time.time()
-ndim,nwalkers = args.dim,1000
 
+ndim,nwalkers = args.dim,100
+iters = 1e3							# Number of iterations each walker goes through
 
 
 np.savetxt("test_actual_data_{}.txt".format(date_append),np.transpose([t,samp]),fmt=['%.5f','%.5f'],
 			header="Freq={}\ttau={}\tamp={}\tgamma={}\txi={}".format(seed_freq,seed_tau,seed_amp,seed_gamma,seed_xi))
 
-pos = [guess+1e-4*rnd.randn(ndim) for i in range (nwalkers)]
+pos = [guess+1e-4*rnd.randn(ndim) for i in range (nwalkers)]			# Initialising the walkers
 
-sampler = mc.EnsembleSampler(nwalkers,ndim,mf.lnprob,args = (t,samp,mf.f2_sin),threads=4)
-sampler.run_mcmc(pos, iters)
-current_time1 = time.time()
-print "EMCEE done, time required:\t", current_time1-current_time
+sampler = mc.EnsembleSampler(nwalkers,ndim,mf.lnprob,args = (t,samp,mf.f2_sin),threads=4)	# Initialising the sampler
+sampler.run_mcmc(pos, iters)																# Running the sampler
+print "EMCEE done, time required:\t", time.time()-current_time
+
+
+## Plotting the samplers
 
 ax1 = plt.subplot(211)
 ax2 = plt.subplot(212)
-#ax3 = plt.subplot(313)
-freq_list = sampler.chain[:, :, 0].flatten()
-tau_list = sampler.chain[:, :, 1].flatten()
-print np.shape(sampler.chain)
-amp_list = sampler.chain[:, 50:, -1].flatten()
-
-#ax1.hist(freq_list,100)
-#ax2.hist(tau_list,100)
-#ax3.hist(amp_list,100)
-freq_bins = np.linspace(np.min(freq_list),np.max(freq_list),num_bins)
-tau_bins = np.logspace(-3,np.log10(max(tau_list)),num_bins)
-amp_bins = np.logspace(-3,np.log10(max(amp_list)),num_bins)
-#ax2.hist(tau_list,tau_bins)
-#ax3.hist(amp_list,amp_bins)
 for freq in range(np.shape(sampler.chain)[0]):ax1.plot(sampler.chain[freq,:,0],'-k' )
 for tau in range(np.shape(sampler.chain)[0]):ax2.plot(sampler.chain[tau,:,1],'-k')
 ax1.axhline(seed_freq,color='b')
@@ -134,24 +117,15 @@ ax2.set_xlabel(r"Step number")
 
 ax2.set_ylabel(r"$\tau$ samples")
 
-#ax3.set_ylabel("Amp hist")
-freq_list = sampler.chain[:, 50:, 0].flatten()
-tau_list = sampler.chain[:, 50:, 1].flatten()
-amp_list = sampler.chain[:, 50:, -1].flatten()
-hist_2d,x_ed,y_ed = np.histogram2d(freq_list,tau_list,bins=(num_bins,num_bins))
-#ax3.hist2d(freq_list,tau_list,100)
-X,Y = np.meshgrid(x_ed,y_ed)
-#ax3.pcolormesh(X,Y,hist_2d.T)
-#plt.colorbar()
-#~ print np.shape(freq_list)
-if args.dim>2:
-	with open ("mcmc_freq_tau_amp_list_{}.txt".format(date_append),'w') as f: np.savetxt(f,np.transpose([freq_list,tau_list,amp_list]),fmt=['%.3f','%.3f','%.3f'])
+
 plt.savefig("mcmc_2d_sample_{}.png".format(date_append))
-plt.show()
+#plt.show()
 plt.clf()
 
 
-
+if args.dim>2:
+	with open ("mcmc_freq_tau_amp_list_{}.txt".format(date_append),'w') as f: 
+		np.savetxt(f,np.transpose([freq_list,tau_list,amp_list]),fmt=['%.3f','%.3f','%.3f'])
 
 samples = sampler.chain[:, 100:, :].reshape((-1, ndim))
 print "Shape of samples :\t", np.shape(samples)
@@ -161,9 +135,16 @@ if args.dim==3:bins = [num_bins,tau_bins,amp_bins]
 elif args.dim==5:bins = [num_bins,tau_bins,amp_bins,num_bins,num_bins]
 elif args.dim==2:bins = [num_bins,tau_bins]
 
+### Plotting the triangle plot of the parameters
+
+
 fig = cr.corner(samples, labels=labels,
                       truths=truths,
                       bins = bins,
                       fill_contours=True,show_titles=True,title_fmt=".3e",#hist_2d_cmap=plt.get_cmap('cool'),
                       quantiles=[0.16,0.84],verbose=True,color='k')
 fig.savefig("triangle_actual_data_{}.pdf".format(date_append))
+
+
+
+
--- my_function.py
+++ my_function.py
@@ -1,11 +1,15 @@
+## Function file written for MCMC sampling for the Bayesian parameter estimation of 
+# post-merger gravitational wave signal of the merger of 2 Neutron stars
+
+### Author: 		Yash Bhargava, IUCAA 
+### Last updated: 	September 18, 2017
+
+
 import numpy as np
 pi = np.pi
 
+############ Functions corresponding to type of signal
 
-def log_Poisson (D,N):
-	#~ return D**N*np.exp(-D)/math.factorial(N)
-	return N*np.log(D)-D
-	
 def damped_sin(t,theta):
 	"""
 	
@@ -20,15 +24,11 @@ def damped_sin(t,theta):
 		tau				: Damping timescale of signal (in sec)
 		freq    		: Frequency of oscillation (in Hz)
 		amp				: (Optional) Amplitude of signal. Default value 1
-		t_start			: (Optional) Time stamp of when the signal begins. Default value 0
-	
+		
 	Output
 	----------------
 	y				: Required signal
 	
-	Note
-	----------------
-	Due to ordering of arguments the case of optional amplitude and given t_start cannot be used
 		
 	"""
 	theta = np.atleast_1d(theta)
@@ -36,25 +36,19 @@ def damped_sin(t,theta):
 		freq = theta
 		tau=1
 		amp = 1
-		t_start = 0
+		
 	elif len(theta)==2:
 		freq,tau = theta
 		amp = 1
-		t_start = 0
+		
 	elif len(theta)==3:
 		freq,tau,amp = theta
-		t_start = 0
-	else: freq,tau,amp,t_start = theta
-	# Defining default value
-	#if amp==None: amp=1
-	#if t_start==None: t_start=0
-	ind = np.where (t>=t_start)
-	y = np.zeros(len(t))
-	y[ind] = amp*np.exp(-(t[ind]-t_start)/tau)*np.sin(2*pi*freq*(t[ind]-t_start))
+		
+	
+	y= amp*np.exp(-(t)/tau)*np.sin(2*pi*freq*(t))
 	return y
 
 def f2_sin (t,theta):
-#~ tau,freq,gamma=38,xi=-9e2,amp=1,beta=0,t_start=0):
 	"""
 	
 	Parameters
@@ -67,30 +61,24 @@ def f2_sin (t,theta):
 		gamma           : Coefficient of t^2 term inside the sin (Default value=38 for GNH3)
 		xi				: Coefficient of t^3 term inside the sin (Default value=-9e2 for GNH3)
 		beta			: Phase term inside the sin (Default value=0)
-		t_start			: Start point of signal as compared to start of time series (Default value=0, assumes t_start>=t[0])
-	
-	
+			
 	Returns
 	-----------------
 	y				: Signal comprising of only f2 term in  eq of h+ from Bose et al. 2017
 	
 	"""
-	#freq,tau,gamma,xi,amp,beta,t_start=1000,1,38,-9e2,3,0,0 			# gnh3
-	freq,tau,gamma,xi,amp,beta,t_start=1000,1,-3467,2e4,10,0,0			# alf2
+	#freq,tau,gamma,xi,amp,beta=1000,1,38,-9e2,3,0			# gnh3
+	freq,tau,gamma,xi,amp,beta=1000,1,-3467,2e4,0.5,0			# alf2
 	if len(theta)==2:
 		freq,tau = theta
 	elif len(theta)==3:
 		freq,tau,amp = theta
 	elif len(theta)==5:
 		freq,tau,amp,gamma,xi = theta 
-	elif len(theta)==6:
-		freq,tau,amp,gamma,xi,t_start = theta 
-		print freq,tau,amp,gamma,xi,t_start 
 	
 	
-	ind = np.where (t>=t_start)
-	y = np.zeros(len(t))
-	y[ind] = amp*np.exp(-(t[ind]-t_start)/tau)*np.sin(2*pi*(freq*(t[ind]-t_start)+gamma*(t[ind]-t_start)**2+xi*(t[ind]-t_start)**3)+pi*beta)
+	
+	y = amp*np.exp(-(t)/tau)*np.sin(2*pi*(freq*(t)+gamma*(t)**2+xi*(t)**3)+pi*beta)
 	
 	return y
 
@@ -150,10 +138,7 @@ def single_sin(t,theta):
 	----------------
 	y				: Required signal
 	
-	Note
-	----------------
-	Due to ordering of arguments the case of optional amplitude and given t_start cannot be used
-		
+			
 	"""
 	theta = np.atleast_1d(theta)
 	freq,amp = 1000,1
@@ -164,132 +149,7 @@ def single_sin(t,theta):
 	return y
 
 
-def log_likelihood_v1 (t_k,N_k,*par):
-	
-	"""
-	Assumes a poisson process for generating counts. Not possible if we have Gaussian noise of zero mean
-	"""
-	if len(par)==2: tau,freq,amp,phase = par[0],par[1],1,0
-	elif len(par)==3: tau,freq,amp,phase = par[0],par[1],par[2],0
-	elif len(par)==4: tau,freq,amp,phase = par[0],par[1],par[2],par[3]
-	D = damped_sin(t_k,tau,freq,amp,phase)
-	ll = np.sum(log_Poisson(D,N_k))
-	return -ll
-	
-
-def prior_info_v1 (theta):
-	
-	if theta > 0: return 0
-	return -np.inf
-
-
-def mcmc_1d(t,samp,init_guess = 2000,iter_number = 1e4,step = 100, multi_thres = 50,boost = 0.1):
-	
-	"""
-	Input
-	--------------
-	t				: X axis of the data
-	samp			: Y axis of the data
-	init_guess 		: Initial guess of frequency (Default value=2000)
-	iter_number 	: Number of sampling to be done (Default value = 1e4)
-	step 			: Width of the gaussian around the guess (Default value = 100)
-	multi_thres 	: Number of allowed steps to be fixed at the same value (Default value = 50)
-	boost 			: The fraction by which the step is modified if multi_thres is reached (Default value = 0.1)
-	
-	Output 
-	--------------
-	guess_list		: Guesses of the frequency (len of this array is iter_number)
-	step_list		: List of the steps throughout the sampling
-	
-	"""
-	dt = t[1]-t[0]
-	nyq_freq = 0.5/dt
-	xf = np.linspace (0,nyq_freq,len(t)/2)
-	df = xf[1]-xf[0]
-	sf = np.fft.rfft(samp)
-	
-	current_guess = init_guess
-	multi_count = 0
-	
-	init_step = step
-	# List of parameters saved in loop
-	guess_list = []
-	accept_list = []
-	step_list = []
-
-	for i in range(int(iter_number)):
-		h_current = single_sin(t,current_guess)
-		ll_current = prior_info_v1(current_guess)+likelihood_estimator(sf,h_current,df)
-	
-		new_guess = np.random.normal(current_guess,step)
-		#while new_freq_guess<0: new_freq_guess = rnd.normal(current_freq_guess,freq_step)
-		h_new = single_sin(t,new_guess)
-		ll_new = prior_info_v1(new_guess)+likelihood_estimator(sf,h_new,df)
-
-	
-		ll_diff = ll_new-ll_current
-		if ll_diff<0: p_accept = np.exp(ll_diff)
-		else:p_accept = 1
-		accept = np.random.rand()<p_accept
-	
-		# alternate condition
-	#	p_accept = (ll_diff)
-	#	accept = p_accept>=0
-	
-		if accept:
-			current_guess = new_guess
-#~ 			multi_count = 0
-#~ 			step *= (1-2*boost)
-#~ 		else :
-#~ 			multi_count +=1
-#~ 		if multi_count==multi_thres and step<=0.5*np.max(guess_list): 
-#~ 			step *= 1+boost
-#~ 			multi_count=0
-#~ 		elif multi_count==multi_thres and step>0.5*np.max(guess_list): 
-#~ 			step = init_step
-#~ 			multi_count=0
-	
-		guess_list.append(current_guess)
-		accept_list.append(ll_diff)
-		step_list.append(step)
-	
-	return guess_list,step_list
-
-	
-
-def likelihood_estimator (sf,h,df):
-	"""
-	Input 
-	---------------
-	s				: FFT of  signal 
-	h				: Modelled signal
-	df 				: Minimum frequency diff in FFT
-	
-	Output
-	---------------
-	ll				: Log likehood of the signal being modelled correctly and noise being gaussian
-	
-	"""
-	hf = np.fft.rfft(h)
-	sh = 0.5*( sf*np.conjugate(hf)+ hf*np.conjugate(sf)-hf*np.conjugate(hf))
-	ll = np.real(np.sum(sh)*df)
-	
-	return ll
-
-
-
-def lnlike_v1 (theta,t,s):
-	dt 	= t[1]-t[0]
-	nyq_freq = 0.5/dt
-	xf 	= np.linspace (0,nyq_freq,len(t)/2)
-	df 	= xf[1]-xf[0]
-	sf 	= np.fft.rfft(s)
-	h 	= single_sin(t,theta)
-	hf 	= np.fft.rfft(h)
-	sh 	= 0.5*( sf*np.conjugate(hf)+ hf*np.conjugate(sf)-hf*np.conjugate(hf))
-	ll 	= np.real(np.sum(sh)*df)
-	return ll
-	
+######### The functions for the bayesian analysis
 
 def prior_info_dd (theta):
 	amp_min,amp_max = 1e-5,20
@@ -327,16 +187,7 @@ def prior_info_dd (theta):
 
 
 def lnlike_dd (theta,t,s,func):
-	#~ if len(theta)==2:
-		#~ freq,tau = theta
-		#~ amp = 1
-		#~ gamma = None
-		#~ xi = None	
-	#~ elif len(theta)==3 : 
-		#~ freq,tau,amp = theta
-		#~ gamma = None
-		#~ xi = None
-	#~ elif len(theta)==5 : freq,tau,gamma,xi,amp = theta
+	
 	dt 	= t[1]-t[0]
 	nyq_freq = 0.5/dt
 	xf 	= np.linspace (0,nyq_freq,len(t)/2)
